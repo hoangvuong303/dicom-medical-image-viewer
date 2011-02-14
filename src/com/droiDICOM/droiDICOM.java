814a60fdc63d594dd6ab3d7ff807a74d0b3569d0
mjhardee@gmail.com <mjhardee@gmail.com@6c61d047-0665-08d3-9af1-34a6f6519d74>
2011-02-14 21:58:04 +0000

    Added browse DICOM files prototype.  Buttons now function.

--- src/com/droiDICOM/droiDICOM.java
+++ src/com/droiDICOM/droiDICOM.java
@@ -1,10 +1,13 @@
 package com.droiDICOM;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class droiDICOM extends Activity {
+public class droiDICOM extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -12,9 +15,31 @@ public class droiDICOM extends Activity {
         setContentView(R.layout.main);
         
         Button browseButton = (Button) findViewById(R.id.browse_button);
+        browseButton.setOnClickListener(this);
         Button aboutButton = (Button)findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
         Button exitButton = (Button) findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View v) {
+		Intent i;
+		switch(v.getId()) {
+		case R.id.browse_button:
+			i = new Intent(this, BrowseFiles.class);
+			startActivity(i);
+			break;
+		case R.id.about_button:
+			i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		case R.id.exit_button:
+			this.finish();
+			break;
+		}
+		
+	}
     
     
     
